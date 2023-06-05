0ed78f6467451fd5c492711e5e2406a03bceb782
thuyldo <106019035+thuyldo@users.noreply.github.com>
2023-06-05 10:28:58 +0000

    Chap0_tobegin.R

--- Chap0_tobegin.R
+++ Chap0_tobegin.R
@@ -15,11 +15,5 @@ library(gridExtra)
 
 # load data
 
-demo <- read_sas("Datasphère/zip/AllProvidedFiles_265/demo.sas7bdat", NULL)
-aeendpt <- read_sas("Datasphère/zip/AllProvidedFiles_265/aeendpt.sas7bdat", NULL)
-asendpt <- read_sas("Datasphère/zip/AllProvidedFiles_265/asendpt.sas7bdat",  NULL)
-medhist <- read_sas("Datasphère/zip/AllProvidedFiles_265/medhist.sas7bdat", NULL)
-respeval <- read_sas("Datasphère/zip/AllProvidedFiles_265/respeval.sas7bdat", NULL)
-radiotx <- read_sas("Datasphère/zip/AllProvidedFiles_265/radiotx.sas7bdat", NULL)
-chemotx <- read_sas("Datasphère/zip/AllProvidedFiles_265/chemotx.sas7bdat", NULL)
-surghist <- read_sas("Datasphère/zip/AllProvidedFiles_265/surghist.sas7bdat", NULL)
+load("data.Rdata")
+aeendpt <- data
